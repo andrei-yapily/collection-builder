7a6d9ee07b5a65748f144add8d5acd6115e0e85c
Andrei <andrei.tuta@yapily.com>
2022-05-10 17:01:34 +0100

    Add folder based logic

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const HEAD = fs.readFileSync('test_src/head.js', { encoding: 'UTF8' });
 // const HEADER2 = fs.readFileSync('test_src/new.js', { encoding: 'UTF8' });
 
 class Handler {
-    constructor(){
+    constructor() {
 
     }
 
@@ -13,37 +13,22 @@ class Handler {
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         );
     }
-    
-    // Converts untagged requests to collections
-    convertRequest(category){
-        console.log('Converting ' +category.name+ ' to collection')
-        var requests = category;
-        var name = requests.name.toLowerCase()
-        name = name.replace('get ', '')
-        name = name.replace('create ', '')
-        name = name.replace(' ', '-')
-        return {
-            id: this.uuidv4(),
-            name, name,
-            item: [requests]
-        }
-    }
-    
-    
-    addEvents(category){
-        console.log('Adding tests to ' +category.name)
+
+    addEvents(category) {
+        console.log('\nAdding tests to group [' + category.name + ']')
         var requests = category.item
-        for(var i=0; i< requests.length; i++){
+        for (var i = 0; i < requests.length; i++) {
             var request = requests[i]
-            console.log('Adding tests to ' +request.name)
+            console.log('Adding tests to request [' + request.name + ']')
             var events = [this.generate_event(request.name, 'test')]
             request.events = events;
         }
     }
-    
+
     // adding events based on listener - accounts for event and request scenario
-    generate_event(request_name, listen){
-        var script_text = HEAD+ this.get_event(request_name);
+    generate_event(request_name, listen) {
+        var mapped = request_name.toLowerCase().replaceAll(' ', '_')
+        var script_text = HEAD + this.get_event(mapped);
         var test = {
             "listen": listen,
             "script": {
@@ -55,9 +40,29 @@ class Handler {
         }
         return test;
     }
-    
-    get_event(request_name){
-        return "console.log(\' Event added\');"
+
+    get_event(request_name) {
+        var folders = ["user", "account", "consents"]
+        var folder = folders.filter(v => request_name.includes(v))[0]
+        var path = './test_src/' + folder + '/' + request_name + '.js'
+        if (fs.existsSync(path)) {
+            console.log("Adding test from " + path)
+            return fs.readFileSync(path, { encoding: 'UTF8' });
+        }
+        return ''
+    }
+
+    convertCollection(result) {
+        var items = result.item
+        for (var i = 0; i < items.length; i++) {
+            var category = items[i]
+            if (category.item != undefined) {
+                this.addEvents(category)
+            }
+        }
+        result.items = items.filter(i => i.item != undefined)
+        // console.log('The collection object is: ', result);
+        fs.writeFileSync('test.json', JSON.stringify(result));
     }
 }
 
--- index.js
+++ index.js
@@ -4,31 +4,24 @@ var Converter = require('openapi-to-postmanv2');
 openapiData = fs.readFileSync('sample.json', { encoding: 'UTF8' });
 
 
-var h=  new Handler();
-fs.unlinkSync('test.json');
+var h = new Handler();
+// fs.unlinkSync('test.json');
 
 Converter.convert({ type: 'string', data: openapiData },
-    {}, (err, conversionResult) => {
+    {
+        requestNameSource: 'fallback',
+        indentCharacter: ' ',
+        requestParametersResolution: 'Example',
+        folderStrategy: 'tags',
+        keepImplicitHeaders: true
+
+    }, (err, conversionResult) => {
         if (!conversionResult.result) {
             console.log('Could not convert', conversionResult.reason);
         }
         else {
             result = conversionResult.output[0].data;
-            var items = result.item
-            for (var i = 0; i < items.length; i++) {
-                var category = items[i]
-                if (category.item != undefined) {
-                    h.addEvents(category)
-                }
-                else {
-                    var converted = h.convertRequest(category)
-                    h.addEvents(converted)
-                    items.push(converted)
-                }
-            }
-            result.items = items.filter(i => i.item != undefined)
-            console.log('The collection object is: ', result);
-            fs.writeFileSync('test.json', JSON.stringify(result));
+            h.convertCollection(result);
         }
     }
 );
